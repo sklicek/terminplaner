bd104370ae1a42fd51ce81ad710c511a7ad25761
Steve Klicek <steveklicek@gmail.com>
2022-01-17 13:16:29 +0100

    Add files via upload

---
+++ include/config.inc.php
@@ -0,0 +1,6 @@
+<?php 
+//Allgemeine Konfiguration
+define("FIRMA_NAME", "Musikverein Musterstadt");
+
+//Konfiguration Sprechstunden
+define("RASTER", 15);	//Rasterung: 15 Minuten
---
+++ include/connect_db.php
@@ -0,0 +1,19 @@
+<?php
+//ENTER YOUR DATABASE CONNECTION INFO BELOW:
+$hostname="localhost";
+$database="terminplaner";
+$username="root";
+$password="ste414#$";
+$port="3306";
+
+//DO NOT EDIT BELOW THIS LINE
+$mysqli = @(new mysqli($hostname, $username, $password, $database));
+if ($mysqli->connect_error) {
+	echo "Fehler bei der Verbindung: " .
+	mysqli_connect_error() . "<hr />";
+	exit();
+}
+if (!$mysqli->set_charset("utf8")) {
+	echo "Fehler beim Laden von UTF8 ". $mysqli->error;
+}
+?> 
